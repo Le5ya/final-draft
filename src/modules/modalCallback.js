4c824045a18a6ec80b7829b002c761bf1e83e4a2
Le6ya <liliatolstyh@gmail.comgit config --global user.email liliatolstyh@gmail.com>
2021-03-23 14:26:51 +0300

    close modal call

--- src/modules/modalCallback.js
+++ src/modules/modalCallback.js
@@ -5,11 +5,19 @@ const callback =() =>{
         modalCallback = document.querySelector('.modal-callback'),
         modalClose = document.querySelector('.modal-close');
 
+ const close = () => {
+   modalOverlay.style.display = 'none';
+   modalCallback.style.display = 'none';
+ }
+
  callbackBtn.addEventListener('click', () => {
     modalOverlay.style.display = 'block';
     modalCallback.style.display = 'block';
   
  });
+
+ modalClose.addEventListener('click', close);
+ modalOverlay.addEventListener('click', close);
  
 };
 
