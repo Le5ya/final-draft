51b54c75a9a43277b09ebc872cfc72c2a0ef2f70
Le6ya <liliatolstyh@gmail.comgit config --global user.email liliatolstyh@gmail.com>
2021-03-25 10:45:51 +0300

    bundle

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@
 
 import modalCallback from './modules/modalCallback';
 import scroll from  './modules/scroll';
+import slider from  './modules/slider';
 
   modalCallback();
   scroll();
+  slider();
 
   
